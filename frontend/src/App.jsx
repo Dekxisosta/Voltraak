e0ba4bfbd2a8674a707f40f9fc8bf0ee45ae4e33
Dekxisosta <rroquxii@gmail.com>
2026-08-29 20:01:37 +0800

    fix: remove unused imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,42 +9,8 @@ import { DensityProvider } from '@/shared/contexts/DensityContext'
 import { LayoutPreferenceProvider } from '@/shared/contexts/LayoutPreferenceContext'
 import { NotificationProvider } from '@/shared/hooks/useNotifications'
 import AppRoutes from '@/routes/AppRoutes'
-import ApiTest from '@/test/ApiTest'
-import AuthFlowTest from '@/test/AuthFlowTest'
 
 export default function App() {
-  // Check if we're in test mode via URL parameter
-  const urlParams = new URLSearchParams(window.location.search)
-  const testMode = urlParams.get('test')
-
-  if (testMode === 'api') {
-    return (
-      <ErrorBoundary>
-        <ThemeProvider>
-          <div className="min-h-screen py-8">
-            <ApiTest />
-          </div>
-        </ThemeProvider>
-      </ErrorBoundary>
-    )
-  }
-
-  if (testMode === 'auth') {
-    return (
-      <ErrorBoundary>
-        <ThemeProvider>
-          <NotificationProvider>
-            <AuthProvider>
-              <div className="min-h-screen py-8">
-                <AuthFlowTest />
-              </div>
-            </AuthProvider>
-          </NotificationProvider>
-        </ThemeProvider>
-      </ErrorBoundary>
-    )
-  }
-
   return (
     <ErrorBoundary>
       <ThemeProvider>
